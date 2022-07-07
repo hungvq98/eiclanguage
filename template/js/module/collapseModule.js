0bbdd707073e0e4cfc97b482fd1058e61d20ec88
hungvq98 <hung.vq98@gmail.com>
2022-07-07 15:54:12 +0700

    page faq

--- template/js/module/collapseModule.js
+++ template/js/module/collapseModule.js
@@ -1,8 +1,21 @@
 export default function () {
     var itemCol = document.querySelectorAll('.collapsejs');
+
     itemCol.forEach(e => {
-        e.querySelector('.collapseHead-js').addEventListener('click', ()=> {
+        e.querySelector('.collapseHead-js').addEventListener('click', () => {
             e.classList.toggle('active');
+
+
+            // e.style.height = e.querySelector('.body-js').clientHeight + 110 + 'px';
+
+        })
+        e.querySelectorAll('.collapseChild-js').forEach(k => {
+            k.addEventListener('click', () => {
+                k.classList.toggle('active');
+                // e.style.height = e.querySelector('.body-js').clientHeight + 100 + 'px'
+            })
         })
     })
 }
+
+
