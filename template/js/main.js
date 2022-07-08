b5480071c0cbd82b1262586e3a9b9da9f07da0b6
Hoang Chuan <hoanggiangchuan@gmail.com>
2022-07-08 15:26:13 +0700

    update ss slider

--- template/js/main.js
+++ template/js/main.js
@@ -1,15 +1,17 @@
 import moduleSwiper from "./module/moduleSwiper.js";
-import textareaModule from "./module/textareaModule.js"
-import collapseModule from "./module/collapseModule.js"
+import textareaModule from "./module/textareaModule.js";
+import collapseModule from "./module/collapseModule.js";
 import menumobileModule from "./module/menumobileModule.js";
-import sidebarMenuModule from "./module/sidebarMenuModule.js"
+import sidebarMenuModule from "./module/sidebarMenuModule.js";
+import PopupModule from "./module/PopupModule.js";
 // import toTopModule from "./module/toTopModule.js"
 
-window.addEventListener("DOMContentLoaded" , ()=> {
+window.addEventListener("DOMContentLoaded", () => {
     moduleSwiper();
     textareaModule();
     collapseModule();
     menumobileModule();
     sidebarMenuModule();
+    PopupModule();
     // toTopModule();
-})
+});
