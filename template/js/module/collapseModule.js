d3461d2f2d0e5acb60a8c4a480ecc2ffb110533a
Hoang Chuan <hoanggiangchuan@gmail.com>
2022-07-08 16:08:39 +0700

    cm

--- template/js/module/collapseModule.js
+++ template/js/module/collapseModule.js
@@ -1,21 +1,17 @@
-export default function () {
-    var itemCol = document.querySelectorAll('.collapsejs');
+export default function() {
+    var itemCol = document.querySelectorAll(".collapsejs");
 
-    itemCol.forEach(e => {
-        e.querySelector('.collapseHead-js').addEventListener('click', () => {
-            e.classList.toggle('active');
-
+    itemCol.forEach((e) => {
+        e.querySelector(".collapseHead-js").addEventListener("click", () => {
+            e.classList.toggle("active");
 
             // e.style.height = e.querySelector('.body-js').clientHeight + 110 + 'px';
-
-        })
-        e.querySelectorAll('.collapseChild-js').forEach(k => {
-            k.addEventListener('click', () => {
-                k.classList.toggle('active');
+        });
+        e.querySelectorAll(".collapseChild-js").forEach((k) => {
+            k.addEventListener("click", () => {
+                k.classList.toggle("active");
                 // e.style.height = e.querySelector('.body-js').clientHeight + 100 + 'px'
-            })
-        })
-    })
+            });
+        });
+    });
 }
-
-
